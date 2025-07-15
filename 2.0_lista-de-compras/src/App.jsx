4c177d570aa4ab7af434a48e972497043073357c
Ale Ivonne <aleivonne.ma@gmail.com>
2025-07-15 16:30:23 -0600

    2da_practica_listadecompra

---
+++ 2.0_lista-de-compras/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import ListaCompras from './components/ListaCompras'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mi Lista de Compras</h1>
+      <ListaCompras />
+    </div>
+  )
+}
+
+export default App
